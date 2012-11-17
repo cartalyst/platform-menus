0ebae316378ca5013118d4fbab67036d370dee0b
Ben Corlett <bencorlett@me.com>
2012-11-17 16:19:55 +1100

    Playing.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

---
+++ extension.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+
+	'name'        => 'Menus',
+	'slug'        => 'platform/menus',
+	'author'      => 'Cartalyst LLC',
+	'description' => 'Manages all menus throughout the website admin.',
+	'version'     => '2.0',
+	'is_core'     => true,
+
+	'dependencies' => array(
+		'platform/menus' => array(
+			'composer' => 'platform/extension-menus',
+		),
+	),
+
+	'routes' => function() {
+
+		Route::any('test', function() {
+
+			echo __FILE__;
+
+		});
+	}
+
+);
