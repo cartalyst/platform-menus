a1730c1a74e14f71d055c65d78dad059faf525aa
Ben Corlett <bencorlett@me.com>
2013-01-10 20:50:14 +1100

    Updating Extension type-casting in extension.php.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- extension.php
+++ extension.php
@@ -117,7 +117,7 @@ return array(
 	| your composer.json file specifies the autoloading logic, 'platform',
 	| where your extension receives convention autoloading based on Platform
 	| standards, or a closure which takes two parameters, first is an instance of
-	| Composer\Autoload\ClassLoader and second is Platform\Foundation\Extensions\Extension.
+	| Composer\Autoload\ClassLoader and second is Platform\Extensions\Extension.
 	| The autoload must set appropriate classes and namespaces available when the
 	| extension is started.
 	|
@@ -152,7 +152,7 @@ return array(
 	|
 	*/
 
-	'boot' => function(Platform\Foundation\Extensions\Extension $extension)
+	'boot' => function(Platform\Extensions\Extension $extension)
 	{
 		// Register a function to set the active menu
 		if ( ! function_exists('set_active_menu'))
@@ -201,12 +201,12 @@ return array(
 	|
 	| Routes closure to be called when the Extension is started. Register any
 	| custom routing logic here. This closure takes a
-	| Platform\Foundation\Extensions\Extension object as a parameter just in case you
+	| Platform\Extensions\Extension object as a parameter just in case you
 	| need it.
 	|
 	*/
 
-	'routes' => function(Platform\Foundation\Extensions\Extension $extension)
+	'routes' => function(Platform\Extensions\Extension $extension)
 	{
 		Route::get(':api/v1/menus/{slug}/children', 'Platform\Menus\Controllers\Api\V1\ChildrenController@show');
 		Route::put(':api/v1/menus/{slug}/children', 'Platform\Menus\Controllers\Api\V1\ChildrenController@update');
