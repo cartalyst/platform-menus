f1c259e4409a0a27f0961a35b09bf5c74f8de336
Ben Corlett <bencorlett@me.com>
2013-04-07 19:33:35 +1000

    Update UI.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- themes/admin/platform/default/packages/platform/menus/views/index.blade.php
+++ themes/admin/platform/default/packages/platform/menus/views/index.blade.php
@@ -7,37 +7,49 @@
 
 <!-- Page content -->
 @section('content')
-<div class="page-header">
-	<h3>
-		{{ Lang::get('platform/menus::general.title') }}
 
-		<div class="pull-right">
-			<a href="{{ URL::to(ADMIN_URI . '/menus/create') }}" class="btn btn-info btn-small">{{ Lang::get('button.create') }}</a>
+<section id="menus">
+
+	<header class="clearfix">
+		<h1>{{ Lang::get('platform/menus::general.title') }}</h1>
+		<nav class="tertiary-navigation">
+			@widget('platform/ui::nav.show', array(2, 1, 'nav nav-pills', ADMIN_URI))
+		</nav>
+	</header>
+
+	<hr>
+
+	<section class="content">
+
+		<div class="actions clearfix">
+			<a class="btn btn-large btn-primary pull-right" href="{{ URL::to(ADMIN_URI.'/menus/create') }}">{{ Lang::get('button.create') }}</a>
 		</div>
-	</h3>
-</div>
 
-<table class="table table-bordered">
-	<thead>
-		<tr>
-			<th>{{ Lang::get('platform/menus::table.name') }}</th>
-			<th>{{ Lang::get('platform/menus::table.slug') }}</th>
-			<th>{{ Lang::get('platform/menus::table.children_count') }}</th>
-			<th class="span2">{{ Lang::get('table.actions') }}</th>
-		</tr>
-	</thead>
-	<tbody>
-	@foreach($menus as $menu)
-		<tr>
-			<td>{{ $menu->name }}</td>
-			<td>{{ $menu->slug }}</td>
-			<td>{{ Lang::get('platform/menus::table.children', array('count' => $menu->getChildrenCount())) }}</td>
-			<td>
-				<a class="btn btn-small" href="{{ URL::to(ADMIN_URI.'/menus/edit/'.$menu->slug) }}">{{ Lang::get('button.edit') }}</a>
-				<a class="btn btn-small btn-danger" href="{{ URL::to(ADMIN_URI.'/menus/delete/'.$menu->slug) }}">{{ Lang::get('button.delete') }}</a>
-			</td>
-		</tr>
-	@endforeach
-	</tbody>
-</table>
+		<table class="table table-bordered">
+			<thead>
+				<tr>
+					<th>{{ Lang::get('platform/menus::table.name') }}</th>
+					<th>{{ Lang::get('platform/menus::table.slug') }}</th>
+					<th>{{ Lang::get('platform/menus::table.children_count') }}</th>
+					<th class="span2">{{ Lang::get('table.actions') }}</th>
+				</tr>
+			</thead>
+			<tbody>
+			@foreach ($menus as $menu)
+				<tr>
+					<td>{{ $menu->name }}</td>
+					<td>{{ $menu->slug }}</td>
+					<td>{{ Lang::get('platform/menus::table.children', array('count' => $menu->getChildrenCount())) }}</td>
+					<td>
+						<a class="btn btn-small" href="{{ URL::to(ADMIN_URI.'/menus/edit/'.$menu->slug) }}">{{ Lang::get('button.edit') }}</a>
+						<a class="btn btn-small btn-danger" href="{{ URL::to(ADMIN_URI.'/menus/delete/'.$menu->slug) }}">{{ Lang::get('button.delete') }}</a>
+					</td>
+				</tr>
+			@endforeach
+			</tbody>
+		</table>
+
+	</section>
+</section>
+
 @stop
