6eb54372d90341e799d190a9cea8e905f6cd33ad
Jared <jared@wearedifrnt.com>
2013-05-17 13:44:25 -0700

    removed data-remote

--- themes/admin/default/packages/platform/menus/views/index.blade.php
+++ themes/admin/default/packages/platform/menus/views/index.blade.php
@@ -117,7 +117,7 @@ jQuery(document).ready(function($){
 											<i class="icon-edit"></i>
 										</a>
 
-										<a data-toggle="modal" data-target="#platform-modal-confirm" data-remote="false" href="{{ URL::toAdmin('menus/delete/[[id]]') }}" class="btn btn-danger" title="Delete">
+										<a data-toggle="modal" data-target="#platform-modal-confirm" href="{{ URL::toAdmin('menus/delete/[[id]]') }}" class="btn btn-danger" title="Delete">
 											<i class="icon-trash"></i>
 										</a>
 									</div>
