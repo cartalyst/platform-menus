c27e374abe3d7806468d55561d4bf8561fe47602
Ben Corlett <bencorlett@me.com>
2013-05-29 16:13:13 +1000

    Remove UI widget.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- themes/admin/default/packages/platform/menus/views/index.blade.php
+++ themes/admin/default/packages/platform/menus/views/index.blade.php
@@ -9,6 +9,8 @@
 {{-- Queue Assets --}}
 {{ Asset::queue('tempo', 'js/vendor/tempo/tempo.js', 'jquery') }}
 {{ Asset::queue('data-grid', 'js/vendor/cartalyst/data-grid.js', 'tempo') }}
+{{ Asset::queue('bootstrap-modal', 'js/vendor/bootstrap/modal.js', 'jquery') }}
+{{ Asset::queue('platform-helpers', 'js/vendor/platform/helpers.js', 'bootstrap-modal') }}
 
 {{-- Inline Scripts --}}
 @section('scripts')
@@ -146,5 +148,29 @@ jQuery(document).ready(function($){
 	</section>
 
 </section>
-@widget('platform/ui::modal.confirm')
+
+<div id="platform-modal-confirm" class="modal hide fade">
+
+	<div class="modal-header">
+
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		<h2>Warning</h2>
+
+	</div>
+
+	<div class="modal-body">
+
+		<h4>You are about to delete this record, do you want to Continue?</h4>
+
+	</div>
+
+	<div class="modal-footer">
+
+		<button class="btn btn-cancel" data-dismiss="modal" aria-hidden="true"><i class="icon-circle-arrow-left"></i> @lang('button.cancel')</button>
+		<a href="#" class="btn btn-primary confirm"><i class="icon-trash"></i> @lang('button.delete') </a>
+
+	</div>
+
+</div>
+
 @stop
