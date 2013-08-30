f6799eadb248b95e53ce519c873fcc6b792ede8a
Igor Herson <igorhaf@gmail.com>
2013-08-30 11:44:25 -0300

    add brazilian portuguese translation

---
+++ lang/pt-br/message.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Part of the Platform application.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @package    Platform
+ * @version    2.0.0
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011 - 2013, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+return array(
+
+	'already_exists' => 'Menu já existe!',
+	'not_found'      => 'Menu [:id] ou não é um item do menu raiz ou não existe.',
+	'no_children'    => 'Sem Nó.',
+
+	'success' => array(
+		'create' => 'O menu foi criado com sucesso.',
+		'update' => 'O menu foi editado com sucesso.',
+		'delete' => 'O menu foi excluido com sucesso.',
+	),
+
+	'error' => array(
+		'create' => 'Houve um problema da criação do menu. Por favor, tente novamente.',
+		'update' => 'Houve um problema da ediçao do menu. Por favor, tente novamente.',
+		'delete' => 'Houve um problema da exclusão do menu. Por favor, tente novamente.',
+	),
+
+);
