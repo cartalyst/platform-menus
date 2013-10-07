40438ea3ec9570f14344fdc3dd6e8d77aaae5fa2
Bruno Gaspar <brunofgaspar1@gmail.com>
2013-10-07 22:05:38 +0100

    fix view widget issue not using the target attribute
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- themes/frontend/default/packages/platform/menus/views/widgets/nav/child.blade.php
+++ themes/frontend/default/packages/platform/menus/views/widgets/nav/child.blade.php
@@ -2,7 +2,7 @@
 $children = $child->getChildren();
 ?>
 <li class="{{ $child->in_active_path ? 'active' : null }}{{ $children ? ' dropdown' : null }}">
-	<a href="{{ URL::to($child->uri) }}"@if ($children) id="drop-{{ $child->slug }}" role="button" class="dropdown-toggle" data-toggle="dropdown"@endif>
+	<a target="{{ $child->target }}" href="{{ URL::to($child->uri) }}"@if ($children) id="drop-{{ $child->slug }}" role="button" class="dropdown-toggle" data-toggle="dropdown"@endif>
 		<i class="{{ $child->class }}"></i>
 		<span>{{ $child->name }}</span>
 		@if ($children)
