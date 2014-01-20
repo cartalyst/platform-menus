8781c9770f48e442a8b53502e455e1cfe52e5d8f
drsii <dan.syme@gmail.com>
2014-01-20 11:25:16 -0800

    added disabled styles.
    Signed-off-by: drsii <dan.syme@gmail.com>

--- themes/admin/default/packages/platform/menus/views/manage/children.blade.php
+++ themes/admin/default/packages/platform/menus/views/manage/children.blade.php
@@ -1,7 +1,12 @@
 <li data-item-id="{{ $child->id }}">
 	<div class="item-handle"><i class="fa fa-reorder"></i></div>
 
-	<div class="item-name{{ $child->enabled ? null : ' disabled' }}" data-item="{{ $child->id }}" data-item-name="{{ $child->id }}">{{ $child->name }}</div>
+	<div class="item-name" data-item="{{ $child->id }}" data-item-name="{{ $child->id }}">
+		{{ $child->name }}
+
+		{{ $child->enabled ? '' : '<span class="item-status"><i class="fa fa-eye-slash"></i></span>' }}
+
+	</div>
 
 	<ol>
 		@if ( ! empty($child) and $children = $child->getChildren())
