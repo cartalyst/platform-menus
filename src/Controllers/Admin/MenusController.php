698c1875a45321b887d8cb3f38c338c3e1edb3b5
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-04-12 17:20:10 +0100

    fix small issue when accessing a root menu with id 0
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- src/Controllers/Admin/MenusController.php
+++ src/Controllers/Admin/MenusController.php
@@ -157,7 +157,7 @@ class MenusController extends AdminController {
 	protected function showForm($mode, $slug = null)
 	{
 		// Do we have a menu identifier?
-		if ($slug)
+		if ($slug || $slug == 0)
 		{
 			// Get the menu information
 			if ( ! $menu = $this->menus->findRoot($slug))
