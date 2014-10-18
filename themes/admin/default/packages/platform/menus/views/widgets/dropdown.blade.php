11095323eb99caf92bbe967bec18548a1906da23
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-10-18 01:24:14 +0100

    fix dropdown widget view
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- themes/admin/default/packages/platform/menus/views/widgets/dropdown.blade.php
+++ themes/admin/default/packages/platform/menus/views/widgets/dropdown.blade.php
@@ -1,5 +1,5 @@
 <select{{ $attributes }}>
-@foreach ($customOptions as $id => $value)
+@foreach ($options as $id => $value)
 <option value="{{ $id }}">{{ $value }}</option>
 @endforeach
 @each('platform/menus::widgets/dropdown/item', $items, 'item')
