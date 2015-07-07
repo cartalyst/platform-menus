882cb60664f29fc54a4864cd4d385662c977fa2e
Suhayb Wardany <me@suw.me>
2015-07-07 22:11:06 +0200

    fix tests
    Signed-off-by: Suhayb Wardany <me@suw.me>
    
    # Conflicts:
    #	composer.json
    
    # Conflicts:
    #	composer.json
    #	tests/MenuModelTest.php
    #	tests/NavWidgetTest.php

--- tests/MenuModelTest.php
+++ tests/MenuModelTest.php
@@ -88,21 +88,6 @@ class MenuModelTest extends IlluminateTestCase
     }
 
     /** @test */
-    public function it_can_retrieve_guarded_attributes()
-    {
-        $guarded = [
-            'lft',
-            'rgt',
-            'menu',
-            'depth',
-            'created_at',
-            'updated_at',
-        ];
-
-        $this->assertSame($guarded, $this->menu->getGuarded());
-    }
-
-    /** @test */
     public function it_can_retrieve_the_menu_type()
     {
         $this->app['platform.menus.manager']->shouldReceive('getType')
@@ -182,7 +167,7 @@ class MenuModelTest extends IlluminateTestCase
             ->andReturn($processor = m::mock('Illuminate\Database\Query\Processors\Processor'));
 
         $processor->shouldReceive('processSelect')
-            ->twice()
+            ->times(3)
             ->andReturn([]);
 
         $connection->shouldReceive('select');
